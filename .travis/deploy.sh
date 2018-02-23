4a922ec184ceb2f2f2de1d234dd10ba88472e2d8
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-24 00:20:12 +0100

    Try again to add CD5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'develop' ]; then
-    echo $(echo $SSH_PRIVATE_KEY | base64 -d) > /tmp/id_rsa
-    chmod 600 /tmp/id_rsa
-    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
-    ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
+    eval "$(ssh-agent -s)"
+    chmod 600 ./deploy.key
+    ssh-add ./deploy.key
+    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
+    ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
 elif [ $TRAVIS_BRANCH == 'master' ]; then
-    echo $(echo $SSH_PRIVATE_KEY | base64 -d) > /tmp/id_rsa
-    chmod 600 /tmp/id_rsa
-    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
-    ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $FOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate --force"
+    eval "$(ssh-agent -s)"c
+    hmod 600 ./deploy.key
+    ssh-add ./deploy.key
+    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
+    ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $FOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate --force"
 fi
