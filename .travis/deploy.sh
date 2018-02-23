4687c058046f801e88cc905a7a741ada98abb033
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-24 00:07:10 +0100

    Try again to add CD4

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-echo $TRAVIS_BRANCH
 if [ $TRAVIS_BRANCH == 'develop' ]; then
-    sudo mkdir -p ~/.ssh
-    sudo echo $(echo $SSH_PRIVATE_KEY | base64 -d) > ~/.ssh/id_rsa
-    sudo chmod 600 ~/.ssh/id_rsa
-    sudo rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
-    sudo ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
+    echo $(echo $SSH_PRIVATE_KEY | base64 -d) > /tmp/id_rsa
+    chmod 600 /tmp/id_rsa
+    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
+    ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
 elif [ $TRAVIS_BRANCH == 'master' ]; then
-    sudo mkdir -p ~/.ssh
-    sudo echo $(echo $SSH_PRIVATE_KEY | base64 -d) > ~/.ssh/id_rsa
-    sudo chmod 600 ~/.ssh/id_rsa
-    sudo rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
-    sudo ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $FOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate --force"
+    echo $(echo $SSH_PRIVATE_KEY | base64 -d) > /tmp/id_rsa
+    chmod 600 /tmp/id_rsa
+    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
+    ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $FOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate --force"
 fi
