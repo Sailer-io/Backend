79f55e4d3d0e856c3884bcace8628419469fd765
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-24 00:23:54 +0100

    Try again to add CD9

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_BRANCH == 'develop' ]; then
     rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
     ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
 elif [ $TRAVIS_BRANCH == 'master' ]; then
-    eval "$(ssh-agent -s)"c
+    eval "$(ssh-agent -s)"
     chmod 600 .travis/deploy.key
     ssh-add .travis/deploy.key
     rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
