875be52c7c675912a4885ceaa89bc0e59bbf5207
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-25 17:55:13 +0100

    Exclude .env from deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,12 +3,12 @@ if [ $TRAVIS_BRANCH == 'develop' ]; then
     eval "$(ssh-agent -s)"
     chmod 600 .travis/deploy.key
     ssh-add .travis/deploy.key
-    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
+    rsync -az --exclude=.git --exclude=.env --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$TFOLDER
     ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $TFOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate:fresh --seed"
 elif [ $TRAVIS_BRANCH == 'master' ]; then
     eval "$(ssh-agent -s)"
     chmod 600 .travis/deploy.key
     ssh-add .travis/deploy.key
-    rsync -az --exclude=.git --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
+    rsync -az --exclude=.git --exclude=.env --exclude=vendor --exclude=tests -e "ssh -o StrictHostKeyChecking=no -l $SSH_USER" ./ $IP:$FOLDER
     ssh -o StrictHostKeyChecking=no $SSH_USER@$IP "cd $FOLDER && composer install --no-dev --optimize-autoloader; php artisan migrate --force"
 fi
