44f706a9fa1aa7d9356016f5219a253b04336a78
Lénaïc Grolleau <lenaic@lenaic.me>
2018-03-08 16:04:17 +0100

    BS-1 Added login

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,14 @@
 */
 
 Route::prefix('auth')->group(function () {
-    Route::get('login', function () {
-        $token = \Tymon\JWTAuth\Facades\JWTAuth::fromUser(\App\User::find(1));
-        $r = new \Illuminate\Http\Response($token);
-
-        return $r->header('Authorization', 'Bearer '.$token);
-    });
-    Route::post('login', function () {
-        $token = \Tymon\JWTAuth\Facades\JWTAuth::fromUser(\App\User::find(1));
-        $r = new \Illuminate\Http\Response();
-
-        return $r->header('Authorization', 'Bearer '.$token);
-    });
-    Route::middleware('jwt.refresh')->get('refresh', function () {
-        return new \Illuminate\Http\Response();
+    Route::post('login', 'AuthController@login');
+    Route::middleware('jwt.refresh')->get('refresh', 'AuthController@refresh');
+    Route::middleware('jwt.auth')->group(function () {
+        Route::get('user', 'UserController@whoami');
     });
 });
 
 Route::middleware('jwt.auth')->group(function () {
-    Route::get('whoami', 'UserController@whoami');
+    Route::get('user', 'UserController@whoami');
     Route::apiResource('nodes', 'NodeController');
 });
